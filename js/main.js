61b1c21258fcf113a3b47b0fd53164fcaadce611
Ruslan Symonenko <symonenkors@gmail.com>
2022-11-06 13:01:02 +0200

    made adaptive for header

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
 $(function() {
-    var mixer = mixitup('.gallery__photos');
+    // var mixer = mixitup('.gallery__photos');
 
-    console.log('yes');
-    $("#mygallery").justifiedGallery(
-        {
-            rowHeight: 280,
-            maxRowsCount: 2,
-            margins: 10,
-        }
-    );
+    // console.log('yes');
+    // $("#mygallery").justifiedGallery(
+    //     {
+    //         rowHeight: 280,
+    //         maxRowsCount: 2,
+    //         margins: 10,
+    //     }
+    // );
 
-    $(".youtube-link").grtyoutube();
+    // $(".youtube-link").grtyoutube();
 
-    $('.blog__slider').slick(
-        {
-            arrows: false,
-            dots: true,
-        }
-    );
+    // $('.blog__slider').slick(
+    //     {
+    //         arrows: false,
+    //         dots: true,
+    //     }
+    // );
+
+    $('.burger__btn').on('click', () => {
+        $('.nav__list').toggleClass('nav__list-active');
+    })
 })
